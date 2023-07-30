bf8b0df9fa961f8ddd2d571a7057c49c87a7cb17
angelocapponcelli <angelo.capponcelli@mail.polimi.it>
2023-07-31 01:31:58 +0200

    nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,6 @@ export default defineNuxtConfig({
   },
   app: {
     // Checking environment variable to decide whether the link
-    baseURL: "/", // Name of your GitHub Repository
+    baseURL: "", // Name of your GitHub Repository
   },
 });
