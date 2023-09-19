03cfc37e9ca6f23dfcd3303ef0aeb71d02646ec2
angelocapponcelli <angelo.capponcelli@mail.polimi.it>
2023-09-19 15:53:58 +0200

    Update projects and link

--- pages/index.vue
+++ pages/index.vue
@@ -50,28 +50,30 @@
                                 </div>
                             </div>
                             <div class="flex-none flex flex-row space-x-inner_space">
-                                <NuxtLink to="/"
+                                <NuxtLink to="https://github.com/angelocapponcelli" target="_blank"
                                     class="basis-1/4 bg-background-light fill-white rounded-card_radius aspect-square p-2 hover:bg-green transition duration-200">
                                     <svg class="h-full w-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                                         <path
                                             d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
                                     </svg>
                                 </NuxtLink>
-                                <NuxtLink to="/"
+                                <NuxtLink
+                                    to="https://it.linkedin.com/in/angelo-capponcelli-9a8657144?trk=public-profile-badge-profile-badge-view-profile-cta"
+                                    target="_blank"
                                     class="basis-1/4 bg-background-light fill-white rounded-card_radius aspect-square p-2 hover:bg-purple transition duration-200">
                                     <svg class="h-full w-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                                         <path
                                             d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" />
                                     </svg>
                                 </NuxtLink>
-                                <NuxtLink to="/"
+                                <NuxtLink to="https://www.instagram.com/angelo.capponcelli/" target="_blank"
                                     class="basis-1/4 bg-background-light fill-white rounded-card_radius aspect-square p-2 hover:bg-pink transition duration-200">
                                     <svg class="h-full w-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                                         <path
                                             d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
                                     </svg>
                                 </NuxtLink>
-                                <NuxtLink to="/"
+                                <NuxtLink to="mailto:angelocapponcelli@gmail.com" target="_blank"
                                     class="basis-1/4 bg-background-light fill-white rounded-card_radius aspect-square p-2 hover:bg-yellow hover:fill-background-light transition duration-200">
                                     <svg class="h-full w-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                                         <path
@@ -88,27 +90,27 @@
         <div class="basis-full bg-background-light rounded-card_radius px-5 py-3 flex justify-between items-center">
             <h2 class="text-3xl font-semibold">Projects</h2>
             <span>
-                <NuxtLink to="/"
+                <!--NuxtLink to="/"
                     class="text-yellow py-2 px-3 rounded-full flex flex-row space-x-1 hover:text-black hover:bg-yellow transition duration-200">
                     <span>See more projects</span>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
                     </svg>
-                </NuxtLink>
+                </NuxtLink-->
             </span>
         </div>
 
         <div class="flex flex-row space-x-inner_space">
-            <NuxtLink to="/"
+            <NuxtLink to="https://polimi-game-collective.itch.io/dark-unknown" target="_blank"
                 class="basis-7/12 group bg-green rounded-card_radius p-5 flex flex-row space-x-inner_space hover:shadow-md hover:transform hover:scale-101 transition duration-1000">
-                <div class="basis-5/12"> <img src="~/assets/img/home-image.jpg"
+                <div class="basis-6/12"> <img src="~/assets/img/dark-unknown.jpg"
                         class=" h-full w-full rounded-card_radius object-cover" /></div>
-                <div class="basis-7/12 relative">
-                    <h2 class="text-3xl"> Project name 1 </h2>
-                    <h3 class="text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                        ullamco.
+                <div class="basis-6/12 relative">
+                    <h2 class="text-3xl">Develop videogame</h2>
+                    <h3 class="text-lg">Developed a Unity-based roguelike video game
+                        called 'Dark Unknown' in collaboration with a team of individuals and our university. Explore the
+                        challenging dungeon in this exciting adventure!
                     </h3>
                     <div
                         class="absolute bottom-0 right-0 py-2 px-3 rounded-full flex flex-row space-x-1 group-hover:text-green group-hover:bg-white transition duration-200">
@@ -121,60 +123,59 @@
                     </div>
                 </div>
             </NuxtLink>
-            <NuxtLink to="/"
+            <div
                 class="basis-5/12 group bg-background-light rounded-card_radius p-5 flex flex-row space-x-inner_space hover:shadow-md hover:transform hover:scale-101 transition duration-1000">
-                <div class="basis-5/12"> <img src="~/assets/img/home-image.jpg"
-                        class=" h-full w-full rounded-card_radius object-cover" /></div>
+                <div class="basis-5/12"> <img src="~/assets/img/IoT.png"
+                        class=" h-full w-full rounded-card_radius object-scale-down" /></div>
                 <div class="basis-7/12 relative">
-                    <h2 class="text-3xl"> Project name 2 </h2>
-                    <h3 class="text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                        ullamco.
+                    <h2 class="text-3xl">IoT</h2>
+                    <h3 class="text-lg">Implement and showcase a network architecture similar to LoraWAN in
+                        TinyOS. Connect network to Node-RED, and periodically transmit data from sensor nodes to Thingspeak
+                        through MQTT.
                     </h3>
-                    <div
+                    <!--div 
                         class="absolute bottom-0 right-0 p-2 rounded-full group-hover:text-background-light group-hover:bg-white transition duration-200">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                             stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round"
                                 d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
                         </svg>
-                    </div>
+                    </div-->
                 </div>
-            </NuxtLink>
+            </div>
         </div>
 
         <div class="flex flex-row space-x-inner_space">
-            <NuxtLink to="/"
+            <div
                 class="basis-5/12 group bg-background-light rounded-card_radius p-5 flex flex-row space-x-inner_space hover:shadow-md hover:transform hover:scale-101 transition duration-1000">
-                <div class="basis-5/12"> <img src="~/assets/img/home-image.jpg"
-                        class=" h-full w-full rounded-card_radius object-cover" /></div>
-                <div class="basis-7/12 relative">
-                    <h2 class="text-3xl"> Project name 1 </h2>
-                    <h3 class="text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                        ullamco.
+                <!--div class="basis-5/12"> <img src="~/assets/img/home-image.jpg"
+                        class=" h-full w-full rounded-card_radius object-cover" /></div-->
+                <div class="basis-12/12 relative">
+                    <h2 class="text-3xl">Data quality</h2>
+                    <h3 class="text-lg">Given a dataset and a classification task, introduce missing data ranging from 0% to
+                        50%. Next, employ imputation techniques to fill in the missing values. Evaluate the accuracy of the
+                        classification task, including the application of machine learning algorithms.
                     </h3>
-                    <div
+                    <!--div
                         class="absolute bottom-0 right-0 p-2 rounded-full group-hover:text-background-light group-hover:bg-white transition duration-200">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                             stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round"
                                 d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
                         </svg>
-                    </div>
+                    </div-->
                 </div>
-            </NuxtLink>
-            <NuxtLink to="/"
+            </div>
+            <div
                 class="basis-7/12 group bg-pink rounded-card_radius p-5 flex flex-row space-x-inner_space hover:shadow-md hover:transform hover:scale-101 transition duration-1000">
-                <div class="basis-5/12"> <img src="~/assets/img/home-image.jpg"
-                        class=" h-full w-full rounded-card_radius object-cover" /></div>
-                <div class="basis-7/12 relative">
-                    <h2 class="text-3xl"> Project name 1 </h2>
-                    <h3 class="text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                        ullamco.
+                <div class="basis-4/12"> <img src="~/assets/img/data-analysis.png"
+                        class=" h-fit w-full rounded-card_radius object-cover" /></div>
+                <div class="basis-8/12 relative">
+                    <h2 class="text-3xl">Big and unstructured data project</h2>
+                    <h3 class="text-lg">The aim of the pro ject is to implement a system that manages and queries a
+                        bibliography database of scientific articles. Using Neo4j, MongoDB and Spark.
                     </h3>
-                    <div
+                    <!--div
                         class="absolute bottom-0 right-0 py-2 px-3 rounded-full flex flex-row space-x-1 group-hover:text-pink group-hover:bg-white transition duration-200">
                         <span>See details</span>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -182,9 +183,9 @@
                             <path stroke-linecap="round" stroke-linejoin="round"
                                 d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
                         </svg>
-                    </div>
+                    </div-->
                 </div>
-            </NuxtLink>
+            </div>
         </div>
 
         <div class="flex flex-row space-x-inner_space">
@@ -227,7 +228,7 @@
                                 class="grow bg-[url('~/assets/img/cyber-eye.png')] bg-contain bg-no-repeat bg-center w-full h-full">
                             </div>
                             <p>
-                                Created academic projects involving computer vision.
+                                Created academic projects involving computer graphics.
                             </p>
                         </div>
                         <div class="flex flex-row space-x-inner_space">
